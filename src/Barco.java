0fffbf9cb87255c772be2f21b0d12354b0aca8e7
Asier <asierchicharrogarcia27@gmail.com>
2025-11-21 13:04:40 +0100

    Implementada clase Barco con run y sincronización

--- src/Barco.java
+++ src/Barco.java
@@ -1,16 +1,54 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Barco {
+public class Barco extends Thread {
 
-    private final List<Pasajero> pasajeros = new ArrayList<>();
 
-    public Barco(List<Pasajero> pasajeros) {
-        this.pasajeros.addAll(pasajeros);
+    private String nombre;
+    private int capacidad;
+    private int tiempoRescate;
+
+
+    private List<Pasajero> pasajeros;
+
+    public Barco(String nombre, int capacidad, int tiempoRescate, List<Pasajero> listaCompartida) {
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.tiempoRescate = tiempoRescate;
+
+        this.pasajeros = listaCompartida;
     }
 
-    public boolean hayPasajeros(){
-        return !pasajeros.isEmpty();
-    }
+    @Override
+    public void run() {
 
+        while (true) {
+            List<Pasajero> rescatadosEnEsteViaje = new ArrayList<>();
+
+            synchronized (pasajeros) {
+                if (pasajeros.isEmpty()) {
+                    break;
+                }
+
+
+                for (int i = 0; i < capacidad; i++) {
+                    if (!pasajeros.isEmpty()) {
+
+                        Pasajero p = pasajeros.remove(0);
+                        rescatadosEnEsteViaje.add(p);
+                    }
+                }
+            }
+
+            if (!rescatadosEnEsteViaje.isEmpty()) {
+                System.out.println("El barco " + nombre + " ha rescatado a: " + rescatadosEnEsteViaje);
+                try {
+                    Thread.sleep(tiempoRescate);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println(nombre + " ha terminado su rescate.");
+    }
 }
